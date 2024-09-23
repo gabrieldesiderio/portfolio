8525a8485f0706973064ec59955a276a596047a3
Gabriel Desidério <gabrieldesd7@gmail.com>
2024-09-23 11:49:46 -0300

    fix: contact section

--- src/app/[locale]/sections/contact.tsx
+++ src/app/[locale]/sections/contact.tsx
@@ -1,31 +1,14 @@
-import Link from 'next/link'
+import { useTranslations } from 'next-intl'
 
 export function Contact() {
+  const t = useTranslations('home.contact')
   return (
     <section>
-      <h2 className="text-xl font-semibold mb-8">Contato</h2>
-      <div className="space-y-3">
-        <p>
-          Tem uma ideia, oportunidade, precisa de um orçamento ou simplesmente
-          deseja conversar?
-        </p>
-        <p>
-          Você pode me encontrar no{' '}
-          <Link
-            href="https://www.linkedin.com/in/gabrieldesiderio/"
-            className="underline"
-            target="_blank"
-          >
-            LinkedIn
-          </Link>{' '}
-          ou{' '}
-          <a href="mailto:gabrieldesd7@gmail.com" className="underline">
-            enviar um e-mail para gabrieldesd7@gmail.com
-          </a>
-          .
-        </p>
-        <p>Ficarei feliz com o seu contato :)</p>
-      </div>
+      <h2 className="text-xl font-semibold mb-8">{t('title')}</h2>
+      <div
+        className="space-y-3 text-muted-foreground content"
+        dangerouslySetInnerHTML={{ __html: t.raw('content') }}
+      />
     </section>
   )
 }
