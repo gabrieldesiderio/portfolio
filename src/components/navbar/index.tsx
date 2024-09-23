173df953210f3bef4f9fb620c6b775801adfa545
Gabriel Desidério <gabrieldesd7@gmail.com>
2024-09-23 07:23:17 -0300

    fix: navbar styles

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,8 @@
 import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
 import Link from 'next/link'
 
+import { Button } from '@/components/ui/button'
+
 import { ModeToggle } from './mode-toggle'
 
 export function Navbar() {
@@ -10,16 +12,28 @@ export function Navbar() {
         <ModeToggle />
       </div>
       <nav className="flex items-center justify-between gap-8 leading-lose">
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-6">
           <Link href="/">Home</Link>
           <Link href="/">Stack</Link>
         </div>
-        <div className="flex items-center gap-4">
-          <Link href="https://www.linkedin.com/in/gabrieldesiderio/">
-            <LinkedInLogoIcon className="size-5" />
+        <div className="flex items-center gap-2">
+          <Link
+            href="https://www.linkedin.com/in/gabrieldesiderio/"
+            target="_blank"
+            passHref
+          >
+            <Button variant="ghost" size="icon">
+              <LinkedInLogoIcon className="size-5" />
+            </Button>
           </Link>
-          <Link href="https://github.com/gabrieldesiderio">
-            <GitHubLogoIcon className="size-5" />
+          <Link
+            href="https://github.com/gabrieldesiderio"
+            target="_blank"
+            passHref
+          >
+            <Button variant="ghost" size="icon">
+              <GitHubLogoIcon className="size-5" />
+            </Button>
           </Link>
         </div>
       </nav>
